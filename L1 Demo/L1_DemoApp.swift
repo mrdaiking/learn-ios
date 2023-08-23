ce68895017b8733007dc8943250370dfc8276361
Cuong Nguyen <cuong@blayn.co.jp>
2023-08-23 15:59:00 +0900

    first commit

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Kỳ Nguyên Đại on R 5/08/19.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
