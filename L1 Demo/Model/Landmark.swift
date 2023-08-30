dcfcd0dffaf6cb62f67f4dc714c369aaefe293d4
Cuong Nguyen <cuong@blayn.co.jp>
2023-08-30 12:08:08 +0900

    implement display favorite feature

--- L1 Demo/Model/Landmark.swift
+++ L1 Demo/Model/Landmark.swift
@@ -9,12 +9,13 @@ import SwiftUI
 import Foundation
 import MapKit
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    var isFavorite: Bool
     
     private var imageName: String
     
--- L1 Demo/Views/LandmarkList.swift
+++ L1 Demo/Views/LandmarkList.swift
@@ -8,18 +8,32 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter{ landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
     var body: some View {
         NavigationView {
-            List(landmarks, id: \.id) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorite only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
         }
         .navigationTitle("Landmarks")
-        
     }
 }
 
